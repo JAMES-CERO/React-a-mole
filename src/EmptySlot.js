ce5e7c791da5b7c5088bd1d6007bc8bde87e23da
James Aules <james_aules@icloud.com>
2023-02-15 21:53:42 -0500

    generating MoleContainers

---
+++ src/EmptySlot.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function EmptySlot(props){
+    return(
+    <div>
+        <h1>Empty Slot</h1>
+    </div>
+    )
+}
+
+export default EmptySlot
---
+++ src/MoleContainer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function MoleContainer(props){
+    return(
+    <div>
+        <h1>Mole Container</h1>
+    </div>
+    );
+}
+
+export default MoleContainer
